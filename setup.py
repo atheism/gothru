e8144081576e35a2c7d29f2ce210caa00be03ba3
Weibing Zhang <atheism.zhang@gmail.com>
2017-08-29 23:02:56 +0800

    test with build fail for a standalone app

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 
 setup (
-    name             = "api",
+    name             = "app",
     version          = "0.1",
     description      = "Example application to be deployed.",
     packages         = find_packages(),
-    install_requires = ["gunicorn"],
+    install_requires = ["pycurl"],
 )
