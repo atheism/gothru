d6e7268676229a1377f6f1cdad4278834b77b186
Weibing <atheism.zhang@gmail.com>
2017-09-01 08:20:41 +0000

    run subprocess with arg list as removing shell=True

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ ss_f = open("/opt/app-root/src/logs/ss.log", "aw+")
 socks_f = open("/opt/app-root/src/logs/socks.log", "aw+")
 
 ## run them ##
-cracker = subprocess.Popen(cracker_cmd, stderr=cracker_f, stdout=cracker_f)
-gost = subprocess.Popen(gost_cmd, stderr=gost_f, stdout=gost_f)
-ss = subprocess.Popen(ss_cmd, stderr=ss_f, stdout=ss_f)
-socks = subprocess.Popen(socks_cmd, stderr=socks_f, stdout=socks_f)
+cracker = subprocess.Popen(cracker_cmd.split(), stderr=cracker_f, stdout=cracker_f)
+gost = subprocess.Popen(gost_cmd.split(), stderr=gost_f, stdout=gost_f)
+ss = subprocess.Popen(ss_cmd.split(), stderr=ss_f, stdout=ss_f)
+socks = subprocess.Popen(socks_cmd.split(), stderr=socks_f, stdout=socks_f)
 
 reload = True
  
@@ -30,13 +30,13 @@ while True:
     ss_f.flush()
     socks_f.flush()
     if ss.poll() != None and os.path.isfile('/opt/app-root/src/.ssh/done') :
-        ss = subprocess.Popen(ss_cmd, shell=True, stderr=ss_f, stdout=ss_f)
+        ss = subprocess.Popen(ss_cmd.split(), stderr=ss_f, stdout=ss_f)
     if socks.poll() != None and os.path.isfile('/opt/app-root/src/.ssh/done') :
-        socks = subprocess.Popen(socks_cmd, shell=True, stderr=socks_f, stdout=socks_f)
+        socks = subprocess.Popen(socks_cmd.split(), stderr=socks_f, stdout=socks_f)
     if reload == True and os.path.isfile('/opt/app-root/src/.ssh/passwd') :
         password = open("/opt/app-root/src/.ssh/passwd", "r").read().strip()
         gost_cmd = '/opt/app-root/src/bin/gost -L socks://:1080 -L ss://rc4-md5:%s@:58443' % password
         gost.kill()
-        gost = subprocess.Popen(gost_cmd, stderr=gost_f, stdout=gost_f)
+        gost = subprocess.Popen(gost_cmd.split(), stderr=gost_f, stdout=gost_f)
         reload = False
     time.sleep(10)
